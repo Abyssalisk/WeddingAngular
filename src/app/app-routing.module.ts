7be7da34c3052dbb765af1ba67d82d2139dd2a7a
abyssalisk <abyssalisk@gmail.com>
2020-01-08 19:15:40 -0700

    added routing and new components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import {HomeEspanolComponent} from './home-espanol/home-espanol.component';
+import {HomeComponent} from './home/home.component';
+import { RsvpComponent } from './rsvp/rsvp.component';
+import { RsvpEspanolComponent } from './rsvp-espanol/rsvp-espanol.component';
+import { RegistryComponent } from './registry/registry.component';
+import { WeddingPartyComponent } from './wedding-party/wedding-party.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'home', component: HomeComponent},
+  { path: 'home-espanol', component: HomeEspanolComponent},
+  { path: 'rsvp', component: RsvpComponent},
+  { path: 'rsvp-espanol', component: RsvpEspanolComponent},
+  { path: 'registry', component: RegistryComponent},
+  { path: 'wedding-party', component: WeddingPartyComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [HomeComponent, HomeEspanolComponent, RsvpComponent, RsvpEspanolComponent, RegistryComponent, WeddingPartyComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { HomeEspanolComponent } from './home-espanol/home-espanol.component';
-import { RsvpComponent } from './rsvp/rsvp.component';
-import { RsvpEspanolComponent } from './rsvp-espanol/rsvp-espanol.component';
+import { RegistryComponent } from './registry/registry.component';
+import { WeddingPartyComponent } from './wedding-party/wedding-party.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    HomeEspanolComponent,
-    RsvpComponent,
-    RsvpEspanolComponent
+    routingComponents,
+    RegistryComponent,
+    WeddingPartyComponent
   ],
   imports: [
     BrowserModule,
